1e8c75d350882dfb443130f959e9df2dde334eb9
HarshNexvisionIX <h.narain@nexvisionix.com>
2026-05-22 15:52:40 +0530

    Update config.py

--- ingestion/config.py
+++ ingestion/config.py
@@ -2,7 +2,7 @@
 # MOCK SAP CONFIG
 # -----------------------------------
 
-SAP_BASE_URL = "http://127.0.0.1:8000"
+SAP_BASE_URL = "https://ap-automation-api.onrender.com"
 
 SAP_USERNAME = "sap_user"
 
@@ -12,6 +12,6 @@ SAP_PASSWORD = "sap_pass"
 # MOCK KEFRON CONFIG
 # -----------------------------------
 
-KEFRON_BASE_URL = "http://127.0.0.1:8000"
+KEFRON_BASE_URL = "https://ap-automation-api.onrender.com"
 
 KEFRON_API_KEY = "mock_kefron_token"
